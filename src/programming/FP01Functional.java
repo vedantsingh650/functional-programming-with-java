2d1f09c7d6a1f60124b64abc8cd31c47f7c49429
vedantsingh650 <vedantsingh650@gmail.com>
2022-11-12 18:14:45 +0000

    Commit made in GITHUB VS CODE

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class FP01Functional { 
 	
 	public static void main(String[] args) {
-		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 4, 12, 15);
 		// printAllNumbersInListFunctional(numbers);
 		//printEvenNumbersInListFunctional(numbers);
 		printSquaresOfEvenNumbersInListFunctional(numbers);
